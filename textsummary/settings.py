181dbf49842b86c5180d67cebf624680aff02bf9
nabe <nkm0423@icloud.com>
2021-05-11 21:12:41 +0900

    settings.py STATICFILES_DIRS修正

--- textsummary/settings.py
+++ textsummary/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'summaryapp/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
